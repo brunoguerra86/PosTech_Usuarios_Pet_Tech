f46dcf9c31423f7c7216e7176b2812c86384796c
brunoguerra <brunoguerra@hotmail.com>
2023-10-14 19:01:33 -0300

    first commit

---
+++ src/main/java/br/com/fiap/api/PosTech_Usuarios_Pet_Tech/PosTechUsuariosPetTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api.PosTech_Usuarios_Pet_Tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTechUsuariosPetTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosTechUsuariosPetTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/api/PosTech_Usuarios_Pet_Tech/PosTechUsuariosPetTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api.PosTech_Usuarios_Pet_Tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosTechUsuariosPetTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
